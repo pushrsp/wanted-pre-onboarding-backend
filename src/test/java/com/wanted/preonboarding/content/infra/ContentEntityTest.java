902669547ca38f98b2fa2ca88c711d4cd1ec4787
PUSHRSP <pushrsp@gmail.com>
2023-08-07 14:16:30 +0900

    #7 - content entity 테스트

---
+++ src/test/java/com/wanted/preonboarding/content/infra/ContentEntityTest.java
@@ -0,0 +1,44 @@
+package com.wanted.preonboarding.content.infra;
+
+import com.wanted.preonboarding.content.domain.Content;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ContentEntityTest {
+    @DisplayName("ContentEntity는 ContentDomain으로 부 생성할 수 있다.")
+    @Test
+    public void can_create_content_entity_from_content_domain() throws Exception {
+        //given
+        Content contentDomain = createContentDomain(null, "content");
+
+        //when
+        ContentEntity contentEntity = ContentEntity.from(contentDomain);
+
+        //then
+        assertThat(contentEntity.getId()).isNull();;
+        assertThat(contentEntity.getContent()).isEqualTo(contentDomain.getContent());
+    }
+
+    @DisplayName("ContentEntity는 ContentDomain으로 컨버팅 할 수 있다.")
+    @Test
+    public void content_entity_can_convert_to_content_domain() throws Exception {
+        //given
+        ContentEntity contentEntity = ContentEntity.from(createContentDomain(20L, "content"));
+
+        //when
+        Content contentDomain = contentEntity.toDomain();
+
+        //then
+        assertThat(contentDomain.getId()).isEqualTo(contentEntity.getId());
+        assertThat(contentDomain.getContent()).isEqualTo(contentEntity.getContent());
+    }
+
+    private Content createContentDomain(Long id, String content) {
+        return Content.builder()
+                .id(id)
+                .content(content)
+                .build();
+    }
+}
