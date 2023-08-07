76fd2687713893d1ab4fa2c574d085a3f717cd38
PUSHRSP <pushrsp@gmail.com>
2023-08-07 14:58:57 +0900

    #9 - user repository 구현

--- src/main/java/com/wanted/preonboarding/user/infra/UserJpaRepository.java
+++ src/main/java/com/wanted/preonboarding/user/infra/UserJpaRepository.java
@@ -2,5 +2,8 @@ package com.wanted.preonboarding.user.infra;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
