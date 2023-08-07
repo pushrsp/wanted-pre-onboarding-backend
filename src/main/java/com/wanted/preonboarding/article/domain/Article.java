65da6923db9d0e982c13931871ee96c60e8cad92
PUSHRSP <pushrsp@gmail.com>
2023-08-07 13:50:59 +0900

    #5 - 도메인, 엔티티, JpaRepository 추가

--- src/main/java/com/wanted/preonboarding/article/domain/Article.java
+++ src/main/java/com/wanted/preonboarding/article/domain/Article.java
@@ -1,4 +1,31 @@
 package com.wanted.preonboarding.article.domain;
 
+import com.wanted.preonboarding.content.domain.Content;
+import com.wanted.preonboarding.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
 public class Article {
+    private Long id;
+    private String title;
+    private User user;
+    private Content content;
+    private LocalDateTime createdTime;
+    private LocalDate createdDate;
+    private LocalDateTime modifiedTime;
+
+    @Builder
+    private Article(Long id, String title, User user, Content content , LocalDateTime createdTime, LocalDate createdDate, LocalDateTime modifiedTime) {
+        this.id = id;
+        this.title = title;
+        this.user = user;
+        this.content = content;
+        this.createdTime = createdTime;
+        this.createdDate = createdDate;
+        this.modifiedTime = modifiedTime;
+    }
 }
---
+++ src/main/java/com/wanted/preonboarding/user/infra/UserEntity.java
@@ -0,0 +1,61 @@
+package com.wanted.preonboarding.user.infra;
+
+import com.wanted.preonboarding.article.infra.ArticleEntity;
+import com.wanted.preonboarding.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "USER")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", length = 70, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "created_time", nullable = false)
+    private LocalDateTime createdTime;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    private List<ArticleEntity> articles = new ArrayList<>();
+
+    @Builder
+    private UserEntity(Long id, String email, String password, LocalDateTime createdTime) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.createdTime = createdTime;
+    }
+
+    public static UserEntity from(User user) {
+        return UserEntity.builder()
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .createdTime(user.getCreatedTime())
+                .build();
+    }
+
+    public User toDomain() {
+        return User.builder()
+                .id(this.id)
+                .email(this.email)
+                .password(this.password)
+                .createdTime(this.createdTime)
+                .build();
+    }
+}
