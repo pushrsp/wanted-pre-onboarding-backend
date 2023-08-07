ee683b2c2fb470a544223dabf362546d0f88b189
PUSHRSP <pushrsp@gmail.com>
2023-08-07 14:59:34 +0900

    #9 - content repository 구현

--- src/main/java/com/wanted/preonboarding/content/infra/ContentRepository.java
+++ src/main/java/com/wanted/preonboarding/content/infra/ContentRepository.java
@@ -1,4 +1,9 @@
 package com.wanted.preonboarding.content.infra;
 
+import com.wanted.preonboarding.content.domain.Content;
+
+import java.util.Optional;
+
 public interface ContentRepository {
+    Optional<Content> findByArticleId(Long articleId);
 }
---
+++ src/main/java/com/wanted/preonboarding/content/infra/ContentRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.wanted.preonboarding.content.infra;
+
+import com.wanted.preonboarding.content.domain.Content;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ContentRepositoryImpl implements ContentRepository {
+    private final ContentJpaRepository contentJpaRepository;
+
+    @Override
+    public Optional<Content> findByArticleId(Long articleId) {
+        return contentJpaRepository.findByArticleId(articleId).map(ContentEntity::toDomain);
+    }
+}
