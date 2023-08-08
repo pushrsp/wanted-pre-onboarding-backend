107d1ad9e2f2a9f9ed21f9f1c0c8f06487f0f403
PUSHRSP <pushrsp@gmail.com>
2023-08-08 13:01:03 +0900

    #15 - 객체간 연관관계 매핑

--- src/main/java/com/wanted/preonboarding/article/domain/Article.java
+++ src/main/java/com/wanted/preonboarding/article/domain/Article.java
@@ -12,20 +12,24 @@ import java.time.LocalDateTime;
 public class Article {
     private Long id;
     private String title;
-    private Member user;
+    private Member member;
     private Content content;
     private LocalDateTime createdTime;
     private LocalDate createdDate;
     private LocalDateTime modifiedTime;
 
     @Builder
-    private Article(Long id, String title, Member user, Content content , LocalDateTime createdTime, LocalDate createdDate, LocalDateTime modifiedTime) {
+    private Article(Long id, String title, Member member, LocalDateTime createdTime, LocalDate createdDate, LocalDateTime modifiedTime) {
         this.id = id;
         this.title = title;
-        this.user = user;
-        this.content = content;
+        this.member = member;
         this.createdTime = createdTime;
         this.createdDate = createdDate;
         this.modifiedTime = modifiedTime;
+    }
+
+    public void addContent(Content content) {
+        this.content = content;
+        this.content.addArticle(this);
     }
 }
