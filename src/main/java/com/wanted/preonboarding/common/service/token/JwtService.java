d052dd0948ee472658b94eb022996cdb902b2446
PUSHRSP <pushrsp@gmail.com>
2023-08-08 17:46:12 +0900

    #19 - 토큰 서비스 추가

---
+++ src/main/java/com/wanted/preonboarding/common/service/token/JwtService.java
@@ -0,0 +1,18 @@
+package com.wanted.preonboarding.common.service.token;
+
+public class JwtService implements TokenService {
+    @Override
+    public String generate() {
+        return null;
+    }
+
+    @Override
+    public String extract() {
+        return null;
+    }
+
+    @Override
+    public String get() {
+        return null;
+    }
+}
---
+++ src/main/java/com/wanted/preonboarding/common/service/token/TokenService.java
@@ -0,0 +1,7 @@
+package com.wanted.preonboarding.common.service.token;
+
+public interface TokenService {
+    String generate();
+    String extract();
+    String get();
+}
