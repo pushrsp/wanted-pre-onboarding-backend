e42fe8758e16ca1d5adb1f181e60179a16a31f8e
PUSHRSP <pushrsp@gmail.com>
2023-08-08 18:01:01 +0900

    #19 - jwt 생성 로직 추가

--- src/main/java/com/wanted/preonboarding/common/service/token/JwtService.java
+++ src/main/java/com/wanted/preonboarding/common/service/token/JwtService.java
@@ -1,9 +1,21 @@
 package com.wanted.preonboarding.common.service.token;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+
 public class JwtService implements TokenService {
     @Override
-    public String generate() {
-        return null;
+    public String generate(String data, String secret, Long offset) {
+        if(offset <= 0) {
+            throw new IllegalArgumentException("offset 0보다 커야 됩니다.");
+        }
+
+        return JWT.create()
+                .withClaim("data", data)
+                .withExpiresAt(new Date(System.currentTimeMillis() + offset))
+                .sign(Algorithm.HMAC256(secret));
     }
 
     @Override
