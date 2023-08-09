2ff86913107f335ca16fcdd4222ad738edd7d316
PUSHRSP <pushrsp@gmail.com>
2023-08-09 13:52:33 +0900

    #25 - 수정 테스트

--- src/test/java/com/wanted/preonboarding/article/service/ArticleServiceTest.java
+++ src/test/java/com/wanted/preonboarding/article/service/ArticleServiceTest.java
@@ -4,6 +4,7 @@ import com.wanted.preonboarding.IntegrationTestSupport;
 import com.wanted.preonboarding.article.domain.Article;
 import com.wanted.preonboarding.article.infra.ArticleRepository;
 import com.wanted.preonboarding.article.service.request.ArticleCreateServiceRequest;
+import com.wanted.preonboarding.article.service.request.ArticleUpdateServiceRequest;
 import com.wanted.preonboarding.common.service.date.DateService;
 import com.wanted.preonboarding.content.infra.ContentRepository;
 import com.wanted.preonboarding.member.domain.Member;
@@ -61,6 +62,58 @@ class ArticleServiceTest extends IntegrationTestSupport {
 
         //then
         assertThat(savedArticle.getId()).isNotNull();
+        assertThat(savedArticle.getTitle()).isEqualTo(title);
+        assertThat(savedArticle.getContent().getId()).isNotNull();
+        assertThat(savedArticle.getContent().getContent()).isEqualTo(content);
+    }
+
+    @DisplayName("ArticleUpdateServiceRequest를 통해 article을 수정할 수 있다.")
+    @Test
+    public void can_update_article_by_ArticleUpdateServiceRequest() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 8, 9, 12, 50);
+        Article savedArticle = saveArticle(now, "title", "content");
+
+        ArticleUpdateServiceRequest request = ArticleUpdateServiceRequest.builder()
+                .title("updatedTitle")
+                .content("updatedContent")
+                .memberId(savedArticle.getMember().getId())
+                .articleId(savedArticle.getId())
+                .build();
+
+        //when
+        Article updatedArticle = articleService.update(request);
+
+        //then
+        assertThat(updatedArticle.getId()).isEqualTo(savedArticle.getId());
+        assertThat(updatedArticle.getTitle()).isEqualTo(request.getTitle());
+        assertThat(updatedArticle.getContent().getContent()).isEqualTo(request.getContent());
+        assertThat(updatedArticle.getModifiedTime()).isAfter(savedArticle.getModifiedTime());
+    }
+
+    private Article saveArticle(LocalDateTime now, String title, String content) {
+        Long memberId = saveMember(createMemberDomain("abc@naver.com", "password", now));
+        ArticleCreateServiceRequest request = ArticleCreateServiceRequest.builder()
+                .memberId(memberId)
+                .title(title)
+                .content(content)
+                .build();
+
+        return articleRepository.save(request.toDomain(getDateService(now)));
+    }
+
+    private DateService getDateService(LocalDateTime now) {
+        return new DateService() {
+            @Override
+            public LocalDate getDate() {
+                return now.toLocalDate();
+            }
+
+            @Override
+            public LocalDateTime getDateTime() {
+                return now;
+            }
+        };
     }
 
     private Long saveMember(Member member) {
