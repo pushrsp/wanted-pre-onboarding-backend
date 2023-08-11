0561d7c916efff8fecd47d99ddef4183ddad2961
PUSHRSP <pushrsp@gmail.com>
2023-08-11 12:28:34 +0900

    #35 - 생성 테스트

--- src/test/java/com/wanted/preonboarding/ControllerTestSupport.java
+++ src/test/java/com/wanted/preonboarding/ControllerTestSupport.java
@@ -3,14 +3,19 @@ package com.wanted.preonboarding;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.preonboarding.article.controller.ArticleController;
 import com.wanted.preonboarding.article.service.ArticleService;
+import com.wanted.preonboarding.common.config.custom.CustomConfig;
+import com.wanted.preonboarding.common.config.web.WebConfig;
+import com.wanted.preonboarding.common.service.token.TokenService;
 import com.wanted.preonboarding.member.controller.MemberController;
 import com.wanted.preonboarding.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {MemberController.class, ArticleController.class})
+@Import(value = {CustomConfig.class, WebConfig.class})
 public class ControllerTestSupport {
     @Autowired
     protected MockMvc mockMvc;
@@ -23,4 +28,7 @@ public class ControllerTestSupport {
 
     @MockBean
     protected ArticleService articleService;
+
+    @MockBean
+    protected TokenService tokenService;
 }
---
+++ src/test/java/com/wanted/preonboarding/article/controller/ArticleControllerTest.java
@@ -0,0 +1,50 @@
+package com.wanted.preonboarding.article.controller;
+
+import com.wanted.preonboarding.ControllerTestSupport;
+import com.wanted.preonboarding.article.controller.request.ArticleCreateControllerRequest;
+import com.wanted.preonboarding.article.domain.Article;
+import com.wanted.preonboarding.article.service.request.ArticleCreateServiceRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class ArticleControllerTest extends ControllerTestSupport {
+    @DisplayName("인증된 유저들만 게시글을 생성할 수 있다.")
+    @Test
+    public void can_create_article_by_only_authorized_member() throws Exception {
+        //given
+        ArticleCreateControllerRequest request = ArticleCreateControllerRequest.builder()
+                .title("title")
+                .content("content")
+                .build();
+
+        given(tokenService.verify(any(), any()))
+                        .willReturn("1");
+
+        Article article = createArticle(1L);
+        given(articleService.save(any(ArticleCreateServiceRequest.class)))
+                .willReturn(article);
+
+        //when then
+        mockMvc.perform(post("/api/articles")
+                        .header("authorization", "token")
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.articleId").value(article.getId()));
+    }
+
+    private static Article createArticle(Long id) {
+        return Article.builder()
+                .id(id)
+                .build();
+    }
+}
