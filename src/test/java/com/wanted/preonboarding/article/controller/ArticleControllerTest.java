eaa3ddd1a6e3c9ed2a0bb6b82a7dcf3594636747
PUSHRSP <pushrsp@gmail.com>
2023-08-11 12:35:18 +0900

    #35 - 수정 테스트

--- src/test/java/com/wanted/preonboarding/article/controller/ArticleControllerTest.java
+++ src/test/java/com/wanted/preonboarding/article/controller/ArticleControllerTest.java
@@ -2,14 +2,17 @@ package com.wanted.preonboarding.article.controller;
 
 import com.wanted.preonboarding.ControllerTestSupport;
 import com.wanted.preonboarding.article.controller.request.ArticleCreateControllerRequest;
+import com.wanted.preonboarding.article.controller.request.ArticleUpdateControllerRequest;
 import com.wanted.preonboarding.article.domain.Article;
 import com.wanted.preonboarding.article.service.request.ArticleCreateServiceRequest;
+import com.wanted.preonboarding.article.service.request.ArticleUpdateServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,6 +38,33 @@ class ArticleControllerTest extends ControllerTestSupport {
         mockMvc.perform(post("/api/articles")
                         .header("authorization", "token")
                         .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.articleId").value(article.getId()));
+    }
+
+    @DisplayName("인증된 유저들만 게시글을 수정할 수 있다.")
+    @Test
+    public void can_update_article_by_only_authorized_member() throws Exception {
+        //given
+        ArticleUpdateControllerRequest request = ArticleUpdateControllerRequest.builder()
+                .content("content")
+                .title("title")
+                .build();
+
+        given(tokenService.verify(any(), any()))
+                .willReturn("1");
+
+        Article article = createArticle(1L);
+        given(articleService.update(any(ArticleUpdateServiceRequest.class)))
+                .willReturn(article);
+
+        //when then
+        mockMvc.perform(patch("/api/articles/"+article.getId())
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .header("authorization", "token")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
