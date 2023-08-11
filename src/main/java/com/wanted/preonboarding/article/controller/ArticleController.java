96fde1ab28c4e9ef91db967549b876f5fe132359
PUSHRSP <pushrsp@gmail.com>
2023-08-11 11:08:25 +0900

    #33 - resolver 추가

--- src/main/java/com/wanted/preonboarding/article/controller/ArticleController.java
+++ src/main/java/com/wanted/preonboarding/article/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.wanted.preonboarding.article.controller.response.ArticleReadOneContro
 import com.wanted.preonboarding.article.controller.response.ArticleWriteControllerResponse;
 import com.wanted.preonboarding.article.service.ArticleService;
 import com.wanted.preonboarding.article.service.request.ArticleDeleteServiceRequest;
+import com.wanted.preonboarding.common.config.argumentresolver.Auth;
 import com.wanted.preonboarding.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +20,20 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping("/api/articles")
-    public ApiResponse<ArticleWriteControllerResponse> create(@Valid @RequestBody ArticleCreateControllerRequest request) {
-        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.save(request.toServiceRequest(1L))));
+    public ApiResponse<ArticleWriteControllerResponse> create(@Valid @RequestBody ArticleCreateControllerRequest request, @Auth Long memberId) {
+        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.save(request.toServiceRequest(memberId))));
     }
 
     @PatchMapping("/api/articles/{articleId}")
     public ApiResponse<ArticleWriteControllerResponse> update(@PathVariable @NotBlank(message = "수정 권한이 존재하지 않습니다.") Long articleId,
-                       @RequestBody ArticleUpdateControllerRequest request) {
-        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.update(request.toServiceRequest(1L, articleId))));
+                                                              @RequestBody ArticleUpdateControllerRequest request,
+                                                              @Auth Long memberId) {
+        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.update(request.toServiceRequest(memberId, articleId))));
     }
 
     @DeleteMapping("/api/articles/{articleId}")
-    public ApiResponse<ArticleWriteControllerResponse> delete(@PathVariable @NotBlank(message = "수정 권한이 존재하지 않습니다.") Long articleId) {
-        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.delete(ArticleDeleteServiceRequest.from(1L, articleId))));
+    public ApiResponse<ArticleWriteControllerResponse> delete(@PathVariable @NotBlank(message = "수정 권한이 존재하지 않습니다.") Long articleId, @Auth Long memberId) {
+        return ApiResponse.ok(ArticleWriteControllerResponse.from(articleService.delete(ArticleDeleteServiceRequest.from(memberId, articleId))));
     }
 
     @GetMapping("/api/articles/{articleId}")
---
+++ src/main/java/com/wanted/preonboarding/common/config/argumentresolver/AuthArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.wanted.preonboarding.common.config.argumentresolver;
+
+import com.wanted.preonboarding.common.service.token.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+    private final String secret;
+    private final TokenService tokenService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAnnotation = parameter.hasParameterAnnotation(Auth.class);
+        boolean isSameType = Long.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasAnnotation && isSameType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return Long.parseLong(tokenService.verify(tokenService.extract(request), secret));
+    }
+}
