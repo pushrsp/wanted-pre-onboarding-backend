f8f219645891841e6ce174b20b760cb1a59e1482
PUSHRSP <pushrsp@gmail.com>
2023-08-13 11:41:54 +0900

    #37 - custom exception 추가

---
+++ src/main/java/com/wanted/preonboarding/common/exception/NoResourceFoundException.java
@@ -0,0 +1,7 @@
+package com.wanted.preonboarding.common.exception;
+
+public class NoResourceFoundException extends RuntimeException {
+    public NoResourceFoundException(String message) {
+        super(message);
+    }
+}
