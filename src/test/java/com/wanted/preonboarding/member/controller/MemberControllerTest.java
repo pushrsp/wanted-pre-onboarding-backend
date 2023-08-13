0562bfe7311607db817570d12a171a225552daaf
PUSHRSP <pushrsp@gmail.com>
2023-08-13 11:59:11 +0900

    #39 - 회원가입, 로그인할 시 이메일 또는 비밀번호를 입력하지 않은 경우 테스트

--- src/test/java/com/wanted/preonboarding/member/controller/MemberControllerTest.java
+++ src/test/java/com/wanted/preonboarding/member/controller/MemberControllerTest.java
@@ -6,7 +6,12 @@ import com.wanted.preonboarding.member.controller.request.MemberLoginControllerR
 import com.wanted.preonboarding.member.service.request.MemberLoginServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.MediaType;
+
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
@@ -36,6 +41,25 @@ class MemberControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.data").isString());
     }
 
+    @ParameterizedTest
+    @MethodSource("provideEmailAndPassword")
+    public void email_and_password_should_not_be_blank_when_signup(String email, String password, String expected) throws Exception {
+        //given
+        MemberCreateControllerRequest request = MemberCreateControllerRequest.builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/members/signup")
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value(expected));
+    }
+
     @DisplayName("MemberLoginControllerRequest를 통해 로그인을 할 수 있다.")
     @Test
     public void can_login_by_MemberLoginControllerRequest() throws Exception {
@@ -56,5 +80,31 @@ class MemberControllerTest extends ControllerTestSupport {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value("true"))
                 .andExpect(jsonPath("$.data").isString());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideEmailAndPassword")
+    public void email_and_password_should_not_be_blank_when_login(String email, String password, String expected) throws Exception {
+        //given
+        MemberLoginControllerRequest request = MemberLoginControllerRequest.builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/members/login")
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value(expected));
+    }
+
+    private static Stream<Arguments> provideEmailAndPassword() {
+        return Stream.of(
+                Arguments.of(null, "password", "이메일을 입력해주세요."),
+                Arguments.of("email", null, "비밀번호를 입력해주세요.")
+        );
     }
 }
