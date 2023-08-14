09e5920492f2c7fe02500bb44593849a19dffe0a
PUSHRSP <pushrsp@gmail.com>
2023-08-14 14:23:14 +0900

    #45 - config 설정

--- src/main/java/com/wanted/preonboarding/common/config/web/WebConfig.java
+++ src/main/java/com/wanted/preonboarding/common/config/web/WebConfig.java
@@ -6,7 +6,11 @@ import com.wanted.preonboarding.common.interceptor.path.PathMatcherInterceptor;
 import com.wanted.preonboarding.common.service.token.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,7 +21,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     private final TokenService tokenService;
 
     @Value("${token.secret}")
@@ -50,5 +54,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .includePathPattern("/api/articles/**", "DELETE");
 
         return pathMatcherInterceptor;
+    }
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/exception/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/exception/500");
+
+        factory.addErrorPages(errorPage404);
+        factory.addErrorPages(errorPage500);
     }
 }
